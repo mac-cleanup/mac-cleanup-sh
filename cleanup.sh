3f0f016656dda927a0d8cbfe5c1b58b01ebefc68
Florian Wartner <f.wartner@avanalabs.de>
2015-04-28 11:39:49 +0200

    Initial commit

---
+++ cleanup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+echo 'Please enter your password to grant sudo-rights:'
+sudo -v
+
+echo 'Empty the Trash on all mounted volumes and the main HDD...'
+sudo rm -rfv /Volumes/*/.Trashes &>/dev/null
+sudo rm -rfv ~/.Trash &>/dev/null
+
+echo 'Clear System Log Files...'
+sudo rm -rfv /private/var/log/asl/*.asl &>/dev/null
+sudo rm -rfv /Library/Logs/DiagnosticReports/* &>/dev/null
+sudo rm -rfv /Library/Logs/Adobe/* &>/dev/null
+rm -rfv ~/Library/Containers/com.apple.mail/Data/Library/Logs/Mail/* &>/dev/null
+rm -rfv ~/Library/Logs/CoreSimulator/* &>/dev/null
+
+echo 'Cleanup iOS Applications...'
+rm -rfv ~/Music/iTunes/iTunes\ Media/Mobile\ Applications/* &>/dev/null
+
+echo 'Remove iOS Device Backups...'
+rm -rfv ~/Library/Application\ Support/MobileSync/Backup/* &>/dev/null
+
+echo 'Cleanup XCode Derived Data and Archives...'
+rm -rfv ~/Library/Developer/Xcode/DerivedData/* &>/dev/null
+rm -rfv ~/Library/Developer/Xcode/Archives/* &>/dev/null
+
+echo 'Cleanup Homebrew Cache...'
+brew cleanup --force -s &>/dev/null
+rm -rfv /Library/Caches/Homebrew/* &>/dev/null
+brew tap --repair &>/dev/null
+
+clear && echo 'Everything is cleaned up!'
