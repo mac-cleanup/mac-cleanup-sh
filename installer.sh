68563bf9463f70f83bafde01d660209b56ff0a92
Tony Deng <wolf.deng@gmail.com>
2016-10-31 19:07:00 +0800

    docs(1.0): update install & uninstall comment
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- installer.sh
+++ installer.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 function install() {
+    echo "Download Mac Cleanup"
     curl -o cleanup https://raw.githubusercontent.com/fwartner/mac-cleanup/master/cleanup.sh
+    echo "Init Mac Cleanup"
     chmod +x cleanup
+    echo "Install Mac Cleanup"
     sudo mv cleanup /usr/local/bin/cleanup
 }
 
 function uninstall() {
+    echo "Uninstall Mac Cleanup"
     sudo rm /usr/local/bin/cleanup
 }
 
